986fe35c1cbfdaae9295c67243be294f2a7253af
RenatoMAP77 <renatomapbusiness@gmail.com>
2024-08-19 16:35:07 -0300

    classe aluno

--- lab1/code/Matriculator.java/src/Aluno.java
+++ lab1/code/Matriculator.java/src/Aluno.java
@@ -1,4 +1,22 @@
-public class Aluno {
+import java.util.List;
+
+public class Aluno extends Usuario {
+    private List<Matricula> matriculas;
+    private final int PERMISSAO = 1;
+    private Curso curso;
+
+    public Aluno(String nome, String email, String senha, Curso curso) {
+        super(nome, email, senha);
+        this.curso = curso;
+    }
+
+    public void adicionarMatricula(Disciplina disciplina) {
+        // se a disciplina não estiver no curso, verificar se ja tem mais de 2 materias nao obrigatoria abertas
+        //instanciar uma nova matricula e adiciona-la
+    }
     
+    public void removerMatricula(Matricula matricula) {
+        // a adicionar
+    }
     
 }
