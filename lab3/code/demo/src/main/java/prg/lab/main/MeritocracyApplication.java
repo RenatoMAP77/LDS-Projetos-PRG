72b526bdee08fbced1c8bb79731159907214b1ba
Druitti <gabriel.afa@outlook.com>
2024-10-07 11:43:34 -0300

    feat: Create Spring Project

---
+++ lab3/code/demo/src/main/java/prg/lab/main/MeritocracyApplication.java
@@ -0,0 +1,13 @@
+package prg.lab.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeritocracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeritocracyApplication.class, args);
+	}
+
+}
